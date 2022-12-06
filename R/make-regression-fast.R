94de529187cff5240bd0282c9a76c2e4c1912e4a
Steven Paul Sanderson II, MPH <spsanderson@users.noreply.github.com>
2022-12-06 14:36:10 -0500

    Update make-regression-fast.R

---
+++ R/make-regression-fast.R
@@ -0,0 +1,25 @@
+#' Generate Model Specification calls to `parsnip`
+#'
+#' @family Model_Generator
+#'
+#' @author Steven P. Sanderson II, MPH
+#'
+#' @details With this function you can generate a tibble output of any regression
+#' model specification and it's fitted `workflow` object.
+#'
+#' @description Creates a list/tibble of parsnip model specifications.
+#'
+#' @param
+#'
+#' @examples
+#'
+#' @return
+#' A list or a tibble.
+#'
+#' @name fast_regression
+NULL
+
+#' @export
+#' @rdname fast_regression
+
+fast_regression
