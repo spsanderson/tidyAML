15b8066e5495343c60d2a2f386f9d7d08800a3ca
Steven Paul Sanderson II, MPH <spsanderson@users.noreply.github.com>
2022-12-06 21:22:04 -0500

    Fixes #7

--- R/make-regression-fast.R
+++ R/make-regression-fast.R
@@ -9,9 +9,24 @@
 #'
 #' @description Creates a list/tibble of parsnip model specifications.
 #'
-#' @param
+#' @param .data The data being passed to the function for the regression problem
+#' @param .rec_obj The recipe object being passed.
+#' @param .parsnip_fns The default is 'all' which will create all possible
+#' regression model specifications supported.
+#' @param .parsnip_eng the default is 'all' which will create all possible
+#' regression model specifications supported.
+#' @param .split_type The default is 'initial_split', you can pass any type of
+#' split supported by `rsample`
+#' @param .split_args The default is NULL, when NULL then the default parameters
+#' of the split type will be executed for the rsample split type.
 #'
 #' @examples
+#' library(recipes)
+#' library(dplyr)
+#'
+#' rec_obj <- recipe(mpg ~ ., data = mtcars)
+#' frt_tbl <- fast_regression(mtcars, rec_obj, .parsnip_eng = c("lm","glm"))
+#' glimpse(frt_tbl)
 #'
 #' @return
 #' A list or a tibble.
@@ -22,4 +37,63 @@ NULL
 #' @export
 #' @rdname fast_regression
 
-fast_regression
+fast_regression <- function(.data, .rec_obj, .parsnip_fns = "all",
+                            .parsnip_eng = "all", .split_type = "initial_split",
+                            .split_args = NULL){
+
+  # Tidy Eval ----
+  call <- list(.parsnip_fns) %>%
+    purrr::flatten_chr()
+  engine <- list(.parsnip_eng) %>%
+    purrr::flatten_chr()
+
+  rec_obj <- .rec_obj
+  split_type <- .split_type
+  split_args <- .split_args
+
+  # Checks ----
+
+  # Get data splits
+  df <- dplyr::as_tibble(.data)
+  splits_obj <- create_splits(
+    .data = df,
+    .split_type = split_type,
+    .split_args = split_args
+  )
+
+  # Generate Model Spec Tbl
+  mod_spec_tbl <- fast_regression_parsnip_spec_tbl(
+    .parsnip_fns = call,
+    .parsnip_eng = engine
+  )
+
+  mod_rec_tbl <- mod_spec_tbl %>%
+    dplyr::mutate(.model_recipe = list(rec_obj))
+
+  mod_tbl <- mod_rec_tbl %>%
+    dplyr::mutate(
+      .wflw = list(
+        workflows::workflow() %>%
+          workflows::add_recipe(.model_recipe[[1]]) %>%
+          workflows::add_model(.model_spec[[1]])
+      )
+    ) %>%
+    dplyr::mutate(
+      .fitted_wflw = list(
+        parsnip::fit(.wflw[[1]], data = rsample::training(splits_obj$splits))
+      )
+    ) %>%
+    dplyr::mutate(
+      .pred_wflw = list(
+        predict(.fitted_wflw[[1]], new_data = rsample::testing(splits_obj$splits))
+      )
+    )
+
+  # Return ----
+  class(mod_tbl) <- c("fst_reg_tbl", class(mod_tbl))
+  attr(mod_tbl, ".parsnip_engines") <- .parsnip_eng
+  attr(mod_tbl, ".parsnip_functions") <- .parsnip_fns
+  attr(mod_tbl, ".split_type") <- .split_type
+  attr(mod_tbl, ".split_args") <- .split_args
+  return(mod_tbl)
+}
--- R/utils-reg-to-parsnip.R
+++ R/utils-reg-to-parsnip.R
@@ -5,9 +5,30 @@
 #' @author Steven P. Sanderson II, MPH
 #'
 #' @details Creates a tibble of parsnip regression model specifications. This will
-#' create a tibble of 27 different regression model specifications which can be
+#' create a tibble of 61 different regression model specifications which can be
 #' filtered. The model specs are created first and then filtered out. This will
-#' only create models for __regression__ problems.
+#' only create models for __regression__ problems. To find all of the supported
+#' models in this package you can visit \url{https://www.tidymodels.org/find/parsnip/}
+#'
+#' @seealso \url{https://parsnip.tidymodels.org/reference/linear_reg.html}
+#' @seealso \url{https://parsnip.tidymodels.org/reference/cubist_rules.html}
+#' @seealso \url{https://parsnip.tidymodels.org/reference/survival_reg.html}
+#' @seealso \url{https://parsnip.tidymodels.org/reference/poisson_reg.html}
+#' @seealso \url{https://parsnip.tidymodels.org/reference/bag_mars.html}
+#' @seealso \url{https://parsnip.tidymodels.org/reference/bag_tree.html}
+#' @seealso \url{https://parsnip.tidymodels.org/reference/bart.html}
+#' @seealso \url{https://parsnip.tidymodels.org/reference/boost_tree.html}
+#' @seealso \url{https://parsnip.tidymodels.org/reference/decision_tree.html}
+#' @seealso \url{https://parsnip.tidymodels.org/reference/gen_additive_mod.html}
+#' @seealso \url{https://parsnip.tidymodels.org/reference/mars.html}
+#' @seealso \url{https://parsnip.tidymodels.org/reference/mlp.html}
+#' @seealso \url{https://parsnip.tidymodels.org/reference/nearest_neighbor.html}
+#' @seealso \url{https://parsnip.tidymodels.org/reference/pls.html}
+#' @seealso \url{https://parsnip.tidymodels.org/reference/rand_forest.html}
+#' @seealso \url{https://parsnip.tidymodels.org/reference/rule_fit.html}
+#' @seealso \url{https://parsnip.tidymodels.org/reference/svm_linear.html}
+#' @seealso \url{https://parsnip.tidymodels.org/reference/svm_poly.html}
+#' @seealso \url{https://parsnip.tidymodels.org/reference/svm_rbf.html}
 #'
 #' @description Creates a tibble of parsnip regression model specifications.
 #'
@@ -46,43 +67,76 @@ fast_regression_parsnip_spec_tbl <- function(.parsnip_fns = "all",
   # Make tibble
   mod_tbl <- dplyr::tibble(
     .parsnip_engine = c(
-      "lm",
-      "brulee",
-      "gee",
-      "glm",
-      "glmer",
-      "glmnet",
-      "gls",
-      "h2o",
-      "keras",
-      "lme",
-      "lmer",
-      "spark",
-      "stan",
-      "stan_glmer",
+      # linear_reg
+      "lm", "brulee", "gee", "glm", "glmer", "glmnet", "gls", "h2o", "keras",
+      "lme", "lmer", "spark", "stan", "stan_glmer",
+      # cubist_rules
       "Cubist",
-      "glm",
-      "gee",
-      "glmer",
-      "glmnet",
-      "h2o",
-      "hurdle",
-      "stan",
-      "stan_glmer",
+      # possion_reg
+      "glm", "gee", "glmer", "glmnet", "h2o", "hurdle", "stan", "stan_glmer",
       "zeroinfl",
-      "survival",
-      "flexsurv",
-      "flexsurvspline"
+      # survival_reg
+      "survival", "flexsurv", "flexsurvspline",
+      # bag_mars
+      "earth",
+      # bag_tree
+      "rpart", "C5.0",
+      # bart
+      "dbart",
+      # boost_tree
+      "xgboost", "C5.0","h2o","lightgbm","mboost","spark",
+      # decision_tree
+      "rpart","C5.0","partykit","spark",
+      # gen_additive_mod
+      "mgcv",
+      # mars
+      "earth",
+      # mlp
+      "nnet","brulee","h2o","keras",
+      # nearest_neighbor
+      "kknn",
+      # pls
+      "mixOmics",
+      # rand_forest
+      "ranger","aorsf","h2o","randomforest","spark","partykit",
+      # rule_fit
+      "xrf","h2o",
+      # svm_linear
+      "LibLineaR","kernlab",
+      # svm_poly
+      "kernlab",
+      # svm_rbf
+      "kernlab"
     ),
     .parsnip_mode = c(
       rep("regression", 24),
-      rep("censored regression", 3)
+      rep("censored regression", 3),
+      rep("regression", 12),
+      rep("censored regression", 1),
+      rep("regression", 14),
+      "censored regression",
+      rep("regression", 6)
     ),
     .parsnip_fns = c(
       rep("linear_reg", 14),
       "cubist_rules",
       rep("poisson_reg",9),
-      rep("survival_reg", 3)
+      rep("survival_reg", 3),
+      "bag_mars",
+      rep("bag_tree",2),
+      "bart",
+      rep("boost_tree",6),
+      rep("decision_tree", 4),
+      "gen_additive_mod",
+      "mars",
+      rep("mlp", 4),
+      "nearest_neighbor",
+      "pls",
+      rep("rand_forest",6),
+      rep("rule_fit",2),
+      rep("svm_linear", 2),
+      "svm_poly",
+      "svm_rbf"
     )
   )
 
